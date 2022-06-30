22509dfe61eb32a37a4078c81a9d2fb79a555f31
Nicolas Huber <nicolasluishuber@gmail.com>
2022-06-30 02:06:49 -0300

    Fix dashboard metrics

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,7 +3,7 @@
         <h3>{{ $t('page.dashboard') }}</h3>
 
         <div class="mt-4">
-            <div v-if="metrics.lenght > 0" class="row">
+            <div v-if="metrics.length > 0" class="row">
                 <div v-for="(metric, index) in metrics" :key="index" class="col-md-4 mb-3">
                     <l-metric :metric="metric" />
                 </div>
